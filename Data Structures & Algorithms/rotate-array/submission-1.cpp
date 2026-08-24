06bcc9742a56cf617c877fd434d18bf9a4485488
Rohit bobli <59439081+Rohitbobli@users.noreply.github.com>
2026-08-24 22:25:52 +0530

    Add: rotate-array - submission-1

---
+++ Data Structures & Algorithms/rotate-array/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+    int n = nums.size();
+    k = k%n;
+    for(int i  = 0;i<k;i++){
+        int temp = nums[0];
+        int temp2;
+        for( int j = 0;j<n;j++){
+            temp2 = temp;
+            temp = nums[(j+1)%n];
+            nums[(j+1)%n] = temp2;
+        }
+    }
+    }
+};
