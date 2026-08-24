b2618b81884094479a54268e5401ef02a5775074
Rohit bobli <59439081+Rohitbobli@users.noreply.github.com>
2026-08-24 22:59:11 +0530

    Add: move-zeroes - submission-1

---
+++ Data Structures & Algorithms/move-zeroes/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        int j = 0;
+        while(i< n && nums[i]!=0){
+                i++;
+            }
+        j = i+1;
+        while(j<n){
+            if( nums[j] != 0){
+                swap(nums[i],nums[j]);
+                nums[j] = 0;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+    }
+};
